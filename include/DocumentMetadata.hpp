660b5d97a3f97269e7d11aad0a7066ffd1f59c54
William Panlener <wpanlener@gmail.com>
2020-02-18 11:30:01 -0600

    Properly template id in DocumentMeta.hpp.

--- include/DocumentMetadata.hpp
+++ include/DocumentMetadata.hpp
@@ -6,7 +6,7 @@
 template <class T>
 class DocumentMetadata {
     std::string filename;
-    int id;
+    T id;
 public:
     DocumentMetadata () {}
     DocumentMetadata(std::string filename, T id) : filename(filename), id(id) { }
