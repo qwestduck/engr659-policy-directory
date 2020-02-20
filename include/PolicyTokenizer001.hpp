b69a4b562e64d5fcdcef0516488af1f971c50dc7
William Panlener <wpanlener@gmail.com>
2020-02-20 02:23:43 -0600

    Implement PolicyTokenizer001.

---
+++ include/PolicyTokenizer001.hpp
@@ -0,0 +1,33 @@
+#ifndef POLICYTOKENIZER001_HPP
+#define POLICYTOKENIZER001_HPP
+
+#include "Tokenizer.hpp"
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+
+template <class T>
+class PolicyTokenizer001: public Tokenizer<T> {
+
+public:
+    PolicyTokenizer001() : Tokenizer<T>("PolicyTokenizer001") { }
+
+    std::vector<T> tokenize(std::wstring fulltext) const;
+};
+
+template <>
+class PolicyTokenizer001<std::wstring>: public Tokenizer<std::wstring> {
+public:
+    PolicyTokenizer001() : Tokenizer<std::wstring>("PolicyTokenizer001") { }
+
+    std::vector<std::wstring> tokenize(std::wstring fulltext) const {
+        std::vector<std::wstring> result;
+
+        std::transform(fulltext.begin(), fulltext.end(), fulltext.begin(), ::tolower);
+        fulltext.erase(std::remove(fulltext.begin(), fulltext.end(), '.'), fulltext.end());
+        boost::split(result, fulltext, boost::is_any_of(L"\t\r\n \"“,()…_:"), boost::token_compress_on);
+
+        return result;
+    }
+};
+
+#endif
