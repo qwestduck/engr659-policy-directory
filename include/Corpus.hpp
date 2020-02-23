3f3c4ec87d3176df9c2a8fe2496c187bd9a0629b
William Panlener <wpanlener@gmail.com>
2020-02-23 15:36:37 -0600

    Add noiseWords to Corpus.

--- include/Corpus.hpp
+++ include/Corpus.hpp
@@ -19,6 +19,13 @@ template <class T>
 class Corpus {
     std::map<T, Document> documents;
     std::array<std::vector<std::wstring>, 4> dictionary;
+    std::vector<std::wstring> noiseWords {
+      L"the", L"of", L"to", L"and", L"or", L"for", L"in", L"be",
+      L"is", L"are", L"that", L"will", L"by", L"with", L"as", L"may", L"on", L"an",
+      L"must", L"this", L"from", L"any", L"all", L"at", L"if", L"have", L"it", L"has",
+      L"such", L"can", L"than", L"through", L"they", L"but", L"also", L"and/or",
+      L"been", L"those", L"do", L"their", L"into", L"so", L"then", L"he", L"he/she"
+    };
     Tokenizer<std::wstring> * tokenizer;
     std::array<Index<std::wstring, T>, 4> index;
 public:
