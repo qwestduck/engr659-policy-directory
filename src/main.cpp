4a814211bef2c4840815e09ce7ccd3ff8f76655f
William Panlener <wpanlener@gmail.com>
2020-02-25 01:12:20 -0600

    Stage src/main.cpp to better demonstrate assignment 1.

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,16 @@ auto main() -> int {
     Corpus<std::string> corpus;
 
     corpus.loadFromCSV("../data/corpus-metadata.csv", "../data/text/");
+    corpus.buildIndexNGram(1);
     corpus.buildIndexNGram(2);
+    corpus.buildIndexNGram(3);
 
     // corpus.normalizeDocumentVectorsNGram(2);
     // corpus.printSimilarDocuments();
 
-    corpus.printSummaryNGram(2);
+    corpus.printSummaryNGramBrief(1);
+    corpus.printSummaryNGramBrief(2);
+    corpus.printSummaryNGramBrief(3);
 
     return 0;
 }
