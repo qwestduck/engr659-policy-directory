173792d23c65aef8e812c7ce720d6442235bb617
William Panlener <wpanlener@gmail.com>
2020-04-26 06:48:35 -0500

    Sort keyword candidates and print the top-n.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,11 @@
 #include <string>
 #include <vector>
 
+struct keywordCandidate {
+    std::wstring term;
+    double score;
+};
+
 auto main() -> int {
     Corpus<std::string> corpus;
 
@@ -15,7 +20,7 @@ auto main() -> int {
 
     const int MAX_NGRAM_SIZE = 3;
     const int TOPN = 10;
-    const double TFIDF_WEIGHT = 0.9;
+    const double TFIDF_WEIGHT = 0.6;
 
     using index_t = Index<std::wstring, std::string>;
     using dictionary_t = std::vector<std::wstring>;
@@ -34,25 +39,39 @@ auto main() -> int {
     for(const auto & doc : corpus.getDocuments()) {
         auto docId = doc.getMetadata().getId();
 
+        auto candidates = std::vector<keywordCandidate>();
+
         for(int i = 0; i < MAX_NGRAM_SIZE; i++) {
             auto tfVector =  indices[i].getNormalizedDocumentVector(docId, dictionaries[i]);
             auto posVector = indices[i].getNormalizedPositionVector(docId, dictionaries[i]);
 
-            auto candidates = baer::algorithm::topN_iv(
-                Math::vectorAdd(
-                    Math::scalarProduct(
-                        Math::hadamardProduct(
-                            tfVector, idfVectors[i]
-                        ), TFIDF_WEIGHT
-                    ),
-                    Math::scalarProduct(posVector, 1 - TFIDF_WEIGHT)
-                ), TOPN
+            auto b = Math::vectorAdd(
+                Math::scalarProduct(
+                    Math::hadamardProduct(
+                        tfVector, idfVectors[i]
+                    ), TFIDF_WEIGHT
+                ),
+                Math::scalarProduct(posVector, 1 - TFIDF_WEIGHT)
             );
 
-            for(const auto &[key, value] : candidates) {
-                std::wcout << dictionaries[i][key] << " " << value << std::endl;
+            for(int j = 0; j < dictionaries[i].size(); j++) {
+                candidates.push_back(keywordCandidate{dictionaries[i][j], b[j]});
             }
         }
+
+        std::sort(candidates.begin(), candidates.end(), [](keywordCandidate a, keywordCandidate b) {
+            return a.score > b.score;
+        });
+
+        std::cout << doc.getMetadata().getId();
+
+        for(int i = 0; i < 5 && i < candidates.size(); i++) {
+            auto kc = candidates[i];
+
+            std::wcout << ",\"" << kc.term << "\"";
+        }
+
+        std::cout << std::endl;
     }
 
     return 0;
