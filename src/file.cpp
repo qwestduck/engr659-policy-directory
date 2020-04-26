541b5cab2fc53da1ac66e187da62ae1bfb824833
William Panlener <wpanlener@gmail.com>
2020-04-26 06:47:40 -0500

    Handle reading UTF-8 documents more gracefully.

--- src/file.cpp
+++ src/file.cpp
@@ -1,12 +1,22 @@
+#include <codecvt>
+#include <locale>
 #include <sstream>
 #include <string>
 
 #include "File.hpp"
 
 auto File::fileContentToWString(const std::string &filename) -> std::wstring {
-    std::wifstream fin(filename);
-    std::wstringstream buffer;
-    buffer << fin.rdbuf();
+    auto f = std::wifstream{filename};
+    f.imbue(std::locale(f.getloc(), new std::codecvt_utf8<wchar_t, 0x10ffff, std::consume_header>));
 
-    return buffer.str();
+    if(f) {
+        std::wstringstream stream;
+        stream << f.rdbuf();
+
+        f.close();
+
+        return stream.str();
+    }
+
+    return std::wstring{};
 }
